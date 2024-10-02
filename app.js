63d938a1885bd941b2d57af21c45e78cf10f1c6d
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-10-03 03:12:03 +0600

    search funtionality done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const categoryBtnContainer = document.querySelector("#category-btn-container");
 const newsContainer = document.querySelector("#news-container");
+const searchInput = document.querySelector("#search-input");
+
 
 const loadCategory = async () => {
     const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
@@ -47,6 +49,16 @@ const loadNews = async (catId) => {
         newsContainer.append(newsDiv);
     })
 }
+
+document.querySelector("#btn-search").addEventListener("click", () => {
+    if(searchInput.value){
+        loadNews(searchInput.value);
+        searchInput.value = "";
+    }
+    else{
+        alert("Enter Category Id for Search...")
+    }
+})
 
 loadNews("01");
 loadCategory();
