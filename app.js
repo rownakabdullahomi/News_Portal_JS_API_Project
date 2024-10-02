228add5dc157d61034ce80d84645249b5c6a750c
rownakabdullahomi <rownakabdullahomi@gmail.com>
2024-10-03 02:46:40 +0600

    categorywise news  fetch and display done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const categoryBtnContainer = document.querySelector("#category-btn-container");
+const newsContainer = document.querySelector("#news-container");
 
 const loadCategory = async () => {
     const response = await fetch("https://openapi.programming-hero.com/api/news/categories");
@@ -9,18 +10,43 @@ const loadCategory = async () => {
         categoryBtnDiv.innerHTML = `
             <button class="btn bg-slate-400">${item.category_name}</button>
         `;
+        categoryBtnDiv.addEventListener("click", () => {
+            loadNews(item.category_id);
+        });
         categoryBtnContainer.append(categoryBtnDiv);
     });
 
 };
 
-const loadNews = async () => {
-    const response = await fetch("https://openapi.programming-hero.com/api/news/category/01");
+
+
+const loadNews = async (catId) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/news/category/${catId}`);
     const data = await response.json();
+
+    newsContainer.innerHTML = "";
+
     data.data.forEach(item => {
-        console.log(item);
+        const newsDiv = document.createElement("div");
+        newsDiv.innerHTML = `
+        <div class="card card-compact h-full shadow-xl pt-2">
+            <figure>
+                <img
+                src="${item.thumbnail_url}"
+                alt="Shoes" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title">${item.title}</h2>
+                <p>${item.details.slice(0, 200)}</p>
+                <div class="card-actions justify-end">
+                <button class="btn btn-primary">Read More...</button>
+                </div>
+            </div>
+        </div>
+        `;
+        newsContainer.append(newsDiv);
     })
 }
 
-loadNews();
+loadNews("01");
 loadCategory();
